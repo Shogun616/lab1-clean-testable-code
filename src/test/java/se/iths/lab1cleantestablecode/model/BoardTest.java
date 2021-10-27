8b36d8a61c7661812e76d42066e278823c15e1eb
Philip Mattsson <philip.mattsson@iths.se>
2021-10-27 13:49:46 +0200

    Added CellTest & Cell

--- src/test/java/se/iths/lab1cleantestablecode/model/BoardTest.java
+++ src/test/java/se/iths/lab1cleantestablecode/model/BoardTest.java
@@ -1,8 +1,6 @@
 package se.iths.lab1cleantestablecode.model;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 
