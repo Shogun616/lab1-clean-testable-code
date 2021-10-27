83b8af2b392d90003a307342058432d80a4f9762
Philip Mattsson <philip.mattsson@iths.se>
2021-10-27 18:09:58 +0200

    Updated BoardController & Board

--- src/main/java/se/iths/lab1cleantestablecode/controller/BoardController.java
+++ src/main/java/se/iths/lab1cleantestablecode/controller/BoardController.java
@@ -1,4 +1,32 @@
 package se.iths.lab1cleantestablecode.controller;
 
+import se.iths.lab1cleantestablecode.model.Board;
+
 public class BoardController {
+
+    private Board board;
+
+    public BoardController(){
+        this.board = new Board();
+    }
+
+    public void newGeneration(){
+        board.newGeneration();
+    }
+
+    public void clearBoard(){
+        this.board = new Board();
+    }
+
+    public void createGosperGliderGun(){
+        board.CreateGosperGliderGun();
+    }
+
+    public void setUpOnMouseEvents(){
+
+    }
+
+    private void handleClickedCell(){
+
+    }
 }
--- src/test/java/se/iths/lab1cleantestablecode/controller/BoardControllerTest.java
+++ src/test/java/se/iths/lab1cleantestablecode/controller/BoardControllerTest.java
@@ -1,4 +1,17 @@
 package se.iths.lab1cleantestablecode.controller;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BoardControllerTest {
+
+    private BoardController controller;
+
+    @BeforeEach
+    void init(){
+        controller = new BoardController();
+    }
+
+
 }
