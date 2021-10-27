a90bcf189c984ea7083881c4a4f937e88ba2b8d2
Philip Mattsson <philip.mattsson@iths.se>
2021-10-27 14:43:35 +0200

    Added BoardController & BoardControllerTest, Updated module-info-java, Main & game-of-life.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,8 @@ module se.iths.lab1cleantestablecode {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens se.iths.lab1cleantestablecode to javafx.fxml;
     exports se.iths.lab1cleantestablecode;
+    opens se.iths.lab1cleantestablecode.model;
     exports se.iths.lab1cleantestablecode.model;
-    opens se.iths.lab1cleantestablecode.model to javafx.fxml;
 }
