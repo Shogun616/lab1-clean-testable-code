75746ae63e9b01d16b8fa9a3798a5f24978eea48
Philip Mattsson <philip.mattsson@iths.se>
2021-10-27 14:15:52 +0200

    Added newGenerationForFliderTest

--- src/main/java/se/iths/lab1cleantestablecode/model/Board.java
+++ src/main/java/se/iths/lab1cleantestablecode/model/Board.java
@@ -1,4 +1,33 @@
 package se.iths.lab1cleantestablecode.model;
 
 public class Board {
+
+    private final int numberOfRows = 45;
+    private final int numberOfColumns = 45;
+    private final Cell[][] boardOfCells;
+
+    public Board() {
+        boardOfCells = new Cell[numberOfRows][numberOfColumns];
+        fillBoardWithCells();
+    }
+
+    public Cell[][] getBoarOfCells() {
+        return boardOfCells;
+    }
+
+    public int getNumberOfRows(){
+        return numberOfRows;
+    }
+
+    public int getNumberOfColumns(){
+        return numberOfColumns;
+    }
+
+    private void fillBoardWithCells() {
+        for (int y = 0; y < numberOfRows; y++){
+            for (int x = 0; x < numberOfColumns; x++){
+                boardOfCells[y][x] = new Cell();
+            }
+        }
+    }
 }
--- src/test/java/se/iths/lab1cleantestablecode/model/BoardTest.java
+++ src/test/java/se/iths/lab1cleantestablecode/model/BoardTest.java
@@ -1,6 +1,8 @@
 package se.iths.lab1cleantestablecode.model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 
@@ -9,5 +11,15 @@ public class BoardTest {
     @BeforeEach
     void init(){
         board = new Board();
+    }
+
+    @Test
+    @DisplayName("Check the next generation of gliders.")
+    void newGenerationForGliderTest(){
+        board.getBoarOfCells()[2][3].setAlive(true);
+        board.getBoarOfCells()[3][4].setAlive(true);
+        board.getBoarOfCells()[4][2].setAlive(true);
+        board.getBoarOfCells()[4][3].setAlive(true);
+        board.getBoarOfCells()[4][4].setAlive(true);
     }
 }
