db6104ce9c2e2c65026bfaf98485ea684bc7e50d
Philip Mattsson <philip.mattsson@iths.se>
2021-10-27 13:51:26 +0200

    Added CellTest void init

--- src/test/java/se/iths/lab1cleantestablecode/model/CellTest.java
+++ src/test/java/se/iths/lab1cleantestablecode/model/CellTest.java
@@ -1,4 +1,15 @@
 package se.iths.lab1cleantestablecode.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class CellTest {
+
+    private Cell cell;
+
+    @BeforeEach
+    void init(){
+        cell = new Cell();
+    }
 }
