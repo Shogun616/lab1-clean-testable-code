c457d5bb7f7458ebc4e9e6264d278b9a021bb3be
Philip Mattsson <philip.mattsson@iths.se>
2021-10-28 18:03:19 +0200

    Updated GameLoop

--- src/main/java/se/iths/lab1cleantestablecode/controller/GameLoop.java
+++ src/main/java/se/iths/lab1cleantestablecode/controller/GameLoop.java
@@ -1,13 +1,27 @@
 package se.iths.lab1cleantestablecode.controller;
 
 import javafx.application.Platform;
+import javafx.scene.control.Button;
+
+import java.util.concurrent.ScheduledExecutorService;
 
 public class GameLoop {
 
+    private boolean isRunning = false;
     private final BoardController controller;
+    private int speed = 100;
+    private ScheduledExecutorService service;
 
     public GameLoop(BoardController controller){
         this.controller = BoardController;
+    }
+
+    public void handleStartBtn(Button startBtn){
+        if(isRunning){
+
+        } else {
+
+        }
     }
 
     private Runnable oneStep(){
