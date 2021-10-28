c66305a333bf1058734d5df52999e96a8423c993
Philip Mattsson <philip.mattsson@iths.se>
2021-10-28 14:34:48 +0200

    Updated BoardView

--- src/main/java/se/iths/lab1cleantestablecode/view/BoardView.java
+++ src/main/java/se/iths/lab1cleantestablecode/view/BoardView.java
@@ -1,5 +1,7 @@
 package se.iths.lab1cleantestablecode.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 import se.iths.lab1cleantestablecode.controller.GameController;
 import se.iths.lab1cleantestablecode.model.Cell;
@@ -8,29 +10,32 @@ public class BoardView {
 
     private final CellView[][] cellViewArray;
     private final int numberOfRows;
-    private final int numberOfColmuns;
+    private final int numberOfColumns;
     private GridPane gridPane = new GridPane();
 
     public BoardView(int numberOfRows, int numberOfColumns) {
         this.numberOfRows = numberOfRows;
-        this.numberOfColmuns = numberOfColumns;
+        this.numberOfColumns = numberOfColumns;
         cellViewArray = new CellView[numberOfRows][numberOfColumns];
     }
 
-
     public CellView[][] getCellViewArray() {
-        return cellViewArray();
+        return cellViewArray;
     }
 
     public void upDateBoardView(Cell[][] boarOfCells) {
     }
 
     private void setUpGrid(){
-        gridPane.setStyle();
-        gridPane.setHgap();
-        gridPane.setVgap();
-        gridPane.setAlignment();
-        gridPane.setPadding();
+        gridPane.setStyle("-fx-background-color: grey");
+        gridPane.setHgap(1);
+        gridPane.setVgap(1);
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setPadding(new Insets(5));
         GameController.StaticMainStage.setCenter(gridPane);
+    }
+
+    private void displayBoardInGridPane(){
+
     }
 }
