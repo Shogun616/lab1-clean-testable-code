4ee916d8e7c7d8bd410fbeae2d458b5e96cd335b
Philip Mattsson <philip.mattsson@iths.se>
2021-10-28 18:00:47 +0200

    Updated GameLoop

--- src/main/java/se/iths/lab1cleantestablecode/controller/GameLoop.java
+++ src/main/java/se/iths/lab1cleantestablecode/controller/GameLoop.java
@@ -1,4 +1,26 @@
 package se.iths.lab1cleantestablecode.controller;
 
+import javafx.application.Platform;
+
 public class GameLoop {
+
+    private final BoardController controller;
+
+    public GameLoop(BoardController controller){
+        this.controller = BoardController;
+    }
+
+    private Runnable oneStep(){
+        return () -> {
+            try {
+                Platform.runLater(controller::newGeneration);
+            } catch (Exception e){
+                try {
+                    throw new Exception(e);
+                } catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        };
+    }
 }
