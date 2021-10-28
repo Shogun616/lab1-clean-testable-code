eebbc304565e16e2192f3ef8fd5c4f53178fccb8
Philip Mattsson <philip.mattsson@iths.se>
2021-10-28 18:14:53 +0200

    Updated GameLoop

--- src/main/java/se/iths/lab1cleantestablecode/controller/GameLoop.java
+++ src/main/java/se/iths/lab1cleantestablecode/controller/GameLoop.java
@@ -3,7 +3,9 @@ package se.iths.lab1cleantestablecode.controller;
 import javafx.application.Platform;
 import javafx.scene.control.Button;
 
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class GameLoop {
 
@@ -18,10 +20,53 @@ public class GameLoop {
 
     public void handleStartBtn(Button startBtn){
         if(isRunning){
-
+            stopGame();
+            startBtn.setText("Start");
         } else {
-
+            startGame();
+            startBtn.setText("Stop");
         }
+    }
+
+    public void handleClearBtn(){
+        if(isRunning){
+            stopGame();
+        }
+    }
+
+    public void changeLoopSpeed(int newSpeed){
+        if(isRunning){
+            stopGame();
+            this.speed = newSpeed;
+            startGame(newSpeed);
+        } else {
+            this.speed = newSpeed;
+        }
+    }
+
+    private void startGame(){
+        startLoop();
+        this.isRunning = true;
+    }
+
+    private void startGame(int newSpeed){
+        startLoop(newSpeed);
+        this.isRunning = true;
+    }
+
+    private void stopGame(){
+        service.shutdown();
+        this.isRunning = false;
+    }
+
+    private void startLoop(){
+        service = Executors.newScheduledThreadPool(1);
+        service.scheduleAtFixedRate(oneStep(), 0, speed, TimeUnit.MILLISECONDS);
+    }
+
+    private void startLoop(int newSpeed){
+        service = Executors.newScheduledThreadPool(1);
+        service.scheduleAtFixedRate(oneStep(), newSpeed, speed, TimeUnit.MILLISECONDS);
     }
 
     private Runnable oneStep(){
