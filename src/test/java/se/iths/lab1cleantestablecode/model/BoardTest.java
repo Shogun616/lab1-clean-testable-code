dd381ce27566a6907764f61a5ec7a36bd640f16b
Philip Mattsson <philip.mattsson@iths.se>
2021-11-05 18:15:30 +0100

    Added void init

--- src/test/java/se/iths/lab1cleantestablecode/model/BoardTest.java
+++ src/test/java/se/iths/lab1cleantestablecode/model/BoardTest.java
@@ -1,4 +1,11 @@
 package se.iths.lab1cleantestablecode.model;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class BoardTest {
+
+    @BeforeEach
+    void init(){
+        board = new Board();
+    }
 }
