6048d9cd71e94fb825c3efa85d43d6f28488a97c
Philip Mattsson <philip.mattsson@iths.se>
2021-11-05 18:39:04 +0100

    Updated Board

--- src/main/java/se/iths/lab1cleantestablecode/model/Board.java
+++ src/main/java/se/iths/lab1cleantestablecode/model/Board.java
@@ -59,4 +59,19 @@ public class Board {
         }
         actualCell.setNeighbours(neighbours);
     }
+
+    public void CreateGosperGliderGun(){
+        fillBoardWithCells();
+        int[][] cords = {{1, 5}, {1, 6}, {2, 5}, {2, 6}, {11, 5},
+                {11, 6}, {11, 7}, {12, 4}, {12, 8}, {13, 3},
+                {13, 9}, {14, 3}, {14, 9}, {15, 6}, {16, 4},
+                {16, 8}, {17, 5}, {17, 6}, {17, 7}, {18, 6},
+                {21, 3}, {21, 4}, {21, 5}, {22, 3}, {22, 4},
+                {22, 5}, {23, 2}, {23, 6}, {25, 1}, {25, 2},
+                {25, 6}, {25, 7}, {35, 3}, {35, 4}, {36, 3}, {36, 4}};
+
+        for(int[] cord : cords){
+            this.boardOfCells[cord[1]][cord[0]].setAlive(true);
+        }
+    }
 }
