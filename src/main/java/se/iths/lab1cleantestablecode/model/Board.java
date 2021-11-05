3f7abc087cd96b3dd2ecd244311f173380ada3b4
Philip Mattsson <philip.mattsson@iths.se>
2021-11-05 18:36:16 +0100

    Updated Board

--- src/main/java/se/iths/lab1cleantestablecode/model/Board.java
+++ src/main/java/se/iths/lab1cleantestablecode/model/Board.java
@@ -10,13 +10,45 @@ public class Board {
         return boardOfCells;
     }
 
+    public int getNumberOfRows(){
+        return numberOfRows;
+    }
+
+    public int getNumberOfColumns(){
+        return numberOfColumns;
+    }
+
     public void newGeneration() {
         for (int y = 0; y < numberOfRows; y++){
             for (int x =0; x < numberOfColumns; x++){
+                countNeighboursForCell(x, y);
+            }
+        }
+        for (int y = 0; y < numberOfRows; y++){
+            for (int x = 0; x < numberOfColumns; x++){
+                Cell actualCell = boardOfCells[y][x];
+
+                if(!actualCell.isAlive() || (actualCell.getNeighbours() != 2 && actualCell.getNeighbours() != 3)){
+                    actualCell.setAlive(!actualCell.isAlive() && actualCell.getNeighbours() == 3);
+                }
             }
         }
     }
 
-    public void countNeighboursForCell(int i, int i1) {
+    public void countNeighboursForCell(int x, int y) {
+        Cell actualCell = boardOfCells[y][x];
+        int neighbours = 0;
+
+        for (int i = -1; i <= 1; i++){
+            for (int j = -1; j <= 1; j++){
+                if (y + i >= 0 && y + i < numberOfRows && x + j >= 0 && x +j < numberOfColumns){
+                    Cell neighboursCell = boardOfCells[y + i][x + j];
+                    if (neighboursCell.isAlive() && neighboursCell != actualCell){
+                        neighbours++;
+                    }
+                }
+            }
+        }
+        actualCell.setNeighbours(neighbours);
     }
 }
