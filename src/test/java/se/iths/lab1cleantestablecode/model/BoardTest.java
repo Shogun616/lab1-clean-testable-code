dbfdc5a57a24a09dc4e143a1a807f1297f4cc9e3
Philip Mattsson <philip.mattsson@iths.se>
2021-11-05 18:49:31 +0100

    Updated BoardTest

--- src/test/java/se/iths/lab1cleantestablecode/model/BoardTest.java
+++ src/test/java/se/iths/lab1cleantestablecode/model/BoardTest.java
@@ -38,16 +38,102 @@ public class BoardTest {
     }
 
     @Test
+    @DisplayName("Check the third generation of glider.")
+    void threeCellTest() {
+        board.getBoardOfCells()[2][3].setAlive(true);
+        board.getBoardOfCells()[3][4].setAlive(true);
+        board.getBoardOfCells()[4][2].setAlive(true);
+
+        board.newGeneration();
+
+        assertFalse(board.getBoardOfCells()[2][3].isAlive());
+        assertFalse(board.getBoardOfCells()[3][4].isAlive());
+        assertFalse(board.getBoardOfCells()[4][2].isAlive());
+    }
+
+    @Test
+    @DisplayName("Check the fourth generation of glider.")
+    void fourCellTest() {
+        board.getBoardOfCells()[2][3].setAlive(true);
+        board.getBoardOfCells()[3][4].setAlive(true);
+        board.getBoardOfCells()[4][2].setAlive(true);
+        board.getBoardOfCells()[4][3].setAlive(true);
+
+        board.newGeneration();
+
+        assertFalse(board.getBoardOfCells()[2][3].isAlive());
+        assertTrue(board.getBoardOfCells()[3][4].isAlive());
+        assertFalse(board.getBoardOfCells()[4][2].isAlive());
+        assertTrue(board.getBoardOfCells()[4][3].isAlive());
+    }
+
+    @Test
+    @DisplayName("Check the fifth generation of glider.")
+    void fiveCellTest() {
+        board.getBoardOfCells()[2][3].setAlive(true);
+        board.getBoardOfCells()[3][4].setAlive(true);
+        board.getBoardOfCells()[4][2].setAlive(true);
+        board.getBoardOfCells()[4][3].setAlive(true);
+        board.getBoardOfCells()[4][4].setAlive(true);
+
+        board.newGeneration();
+
+        assertFalse(board.getBoardOfCells()[2][3].isAlive());
+        assertTrue(board.getBoardOfCells()[3][4].isAlive());
+        assertFalse(board.getBoardOfCells()[4][2].isAlive());
+        assertTrue(board.getBoardOfCells()[4][3].isAlive());
+        assertTrue(board.getBoardOfCells()[4][4].isAlive());
+    }
+
+    @Test
+    @DisplayName("Check the sixth generation of glider.")
+    void sixCellTest() {
+        board.getBoardOfCells()[2][3].setAlive(true);
+        board.getBoardOfCells()[3][4].setAlive(true);
+        board.getBoardOfCells()[4][2].setAlive(true);
+        board.getBoardOfCells()[4][3].setAlive(true);
+        board.getBoardOfCells()[4][4].setAlive(true);
+        board.getBoardOfCells()[5][3].setAlive(true);
+
+        board.newGeneration();
+
+        assertFalse(board.getBoardOfCells()[2][3].isAlive());
+        assertTrue(board.getBoardOfCells()[3][4].isAlive());
+        assertTrue(board.getBoardOfCells()[4][2].isAlive());
+        assertFalse(board.getBoardOfCells()[4][3].isAlive());
+        assertTrue(board.getBoardOfCells()[4][4].isAlive());
+        assertTrue(board.getBoardOfCells()[5][3].isAlive());
+        assertTrue(board.getBoardOfCells()[3][2].isAlive());
+    }
+
+    @Test
+    @DisplayName("Check the seventh generation of glider.")
+    void sevenCellTest() {
+        board.getBoardOfCells()[2][3].setAlive(true);
+        board.getBoardOfCells()[3][4].setAlive(true);
+        board.getBoardOfCells()[4][2].setAlive(true);
+        board.getBoardOfCells()[4][3].setAlive(true);
+        board.getBoardOfCells()[4][4].setAlive(true);
+        board.getBoardOfCells()[5][3].setAlive(true);
+        board.getBoardOfCells()[3][2].setAlive(true);
+
+        board.newGeneration();
+
+        assertTrue(board.getBoardOfCells()[2][3].isAlive());
+        assertTrue(board.getBoardOfCells()[3][4].isAlive());
+        assertTrue(board.getBoardOfCells()[4][2].isAlive());
+        assertFalse(board.getBoardOfCells()[4][3].isAlive());
+        assertTrue(board.getBoardOfCells()[4][4].isAlive());
+        assertTrue(board.getBoardOfCells()[5][3].isAlive());
+        assertTrue(board.getBoardOfCells()[3][2].isAlive());
+    }
+
+    @Test
     @DisplayName("Count the neighbouring cell.")
     void countNeighbourTest(){
-
         board.countNeighboursForCell(1, 2);
         board.countNeighboursForCell(2, 3);
-        board.countNeighboursForCell(3, 4);
-        board.countNeighboursForCell(4, 2);
-        board.countNeighboursForCell(4, 3);
 
         assertFalse(board.getBoardOfCells()[1][2].isAlive());
-
     }
 }
